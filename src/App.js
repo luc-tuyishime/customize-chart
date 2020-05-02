ef2f86c7adb9c3047dd962cd17541cfe5ab1a630
Jean luc <luctunechi45@gmail.com>
2020-05-02 21:36:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactApexChart from "react-apexcharts";
+import { Card } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './chart.scss';
+
+class App extends Component {
+
+  state = {
+    series: [44, 55, 41, 17, 15, 12, 14, 50, 19, 33],
+    options: {
+      dataLabels: {
+        enabled: false
+      },
+      plotOptions: {
+        pie: {
+          customScale: 1,
+          offsetX: 0,
+          offsetY: 0,
+          expandOnClick: true,
+          dataLabels: {
+            offset: 1,
+            minAngleToShowLabel: 10
+          },
+          donut: {
+            size: '35%',
+            background: 'transparent',
+            labels: {
+              show: true,
+              name: {
+                show: true,
+                fontSize: '15px',
+                offsetY: -10
+              },
+              value: {
+                show: true,
+                fontSize: '15px',
+                offsetY: 16,
+                formatter: function (val) {
+                  return val
+                }
+              },
+              total: {
+                show: false,
+                showAlways: false,
+                label: 'Total',
+                fontSize: '22px',
+                color: '#373d3f',
+                formatter: (w) => {
+                  return w.globals.seriesTotals.reduce((a, b) => {
+                    return a + b
+                  }, 0)
+                }
+              }
+            }
+          },
+        }
+      },
+      chart: {
+        type: 'donut',
+      },
+      responsive: [{
+        breakpoint: 580,
+        options: {
+          chart: {
+            width: 500
+          },
+          legend: {
+            position: 'top'
+          }
+        }
+      }]
+    },
+  };
+
+  render() {
+    const { options, series } = this.state;
+    return (
+      <div id="chart">
+        <p className="text-overview">Overview</p>
+        <Card fluid>
+          <Card.Content>
+            <Card.Header>Demo</Card.Header>
+          </Card.Content>
+          <Card.Content>
+            <ReactApexChart className="chart-pie" options={options} series={series} type="donut" />
+          </Card.Content>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default App;
